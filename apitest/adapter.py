1ea59308a0a139c976ab470121fb15291d5fe12c
GuilleFalcon <falconn.guillermo@gmail.com>
2020-10-02 03:36:03 -0300

    Terminal, templates, auth

---
+++ apitest/adapter.py
@@ -0,0 +1,9 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from django.forms import ValidationError
+
+class UsernameMaxAdapter(DefaultAccountAdapter):
+
+    def clean_username(self, username):
+        if len(username) > 10:
+            raise ValidationError('Please enter a username value less than the current one')
+        return DefaultAccountAdapter.clean_username(self,username) # For other default validations
--- apitest/settings/base.py
+++ apitest/settings/base.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
@@ -142,4 +144,4 @@ GRAPHENE = {
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
-
+ACCOUNT_ADAPTER = 'apitest.adapter.UsernameMaxAdapter'
